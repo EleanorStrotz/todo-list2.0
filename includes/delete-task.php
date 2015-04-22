1ef6b679d0328d51acc0ce81012356d8a0d12419
EleanorStrotz <EleanorStrotz@gmail.com>
2015-04-22 11:09:35 -0700

    video 13 done

--- includes/delete-task.php
+++ includes/delete-task.php
@@ -1,6 +1,10 @@
 <?php
 	$task_id = strip_tags($_POST['id']);
 	require('connect.php');
-	$mysqli = new mysqli('localhost','root', 'root', 'todo');)
+	$mysqli = new mysqli('localhost','root', 'root', 'todo');
+
+	if ($result = $mysqli->query("DELETE FROM tasks WHERE id='task_id'")) {
+
+	}
 
 ?>
