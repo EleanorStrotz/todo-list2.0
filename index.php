24399b11db7bcedde1bf1c7d34bad34dacac79c0
EleanorStrotz <EleanorStrotz@gmail.com>
2015-04-22 11:22:21 -0700

    video 16 done

--- index.php
+++ index.php
@@ -19,13 +19,13 @@
 						$task_id = $row['id'];
 						$task_name = $row["task"];
 
-						echo "<li>
-						<span>'.$task_name'
-						";
+					echo '<li>
+					<span>'.$task_name.'</span>
+					<img id= "'.$task_id. '" class="delete-button" width="10px" src="image/close.svg"/>
+					</li>';
 					}
 				}
 			}
-			?>
 
 			?>
 		</ul>
