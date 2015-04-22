ecca9a0d8934bb55dcce317ffbc2de2f203a9095
EleanorStrotz <EleanorStrotz@gmail.com>
2015-04-22 11:18:33 -0700

    video 15 done

--- index.php
+++ index.php
@@ -9,7 +9,25 @@
 	<div class="task-list">
 		<ul>
 		<!-- links index.php to connect.php-->
-			<?php require("includes/connect.php"); ?>
+			<?php require("includes/connect.php"); 
+	$mysqli = new mysqli('localhost', 'root', 'root', 'todo');
+		$query = "SELECT * FROM tasks ORDER BY date ASC, time ASC";
+			if ($result = $mysqli->query($query)) {
+				$numrows = $result->num_rows;
+				if($numrows>0){
+					while($row = $result->fetch_assoc()){
+						$task_id = $row['id'];
+						$task_name = $row["task"];
+
+						echo "<li>
+						<span>'.$task_name'
+						";
+					}
+				}
+			}
+			?>
+
+			?>
 		</ul>
 
 	</div>
