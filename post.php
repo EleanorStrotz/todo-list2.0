619e06c5e848de3a9af01378e08d24f86827c015
EleanorStrotz <EleanorStrotz@gmail.com>
2015-04-24 11:08:02 -0700

    errors fixed

--- post.php
+++
@@ -1,9 +0,0 @@
-<?php
-//links header.php with post.php
-require_once(__DIR__ . "/view/header.php");
-// links form.php with post.php
-require_once(__DIR__ . "/view/form.php");
-// links footer.php with post.php
-require_once(__DIR__ . "/view/footer.php");
-
-?>
--- view/header.php
+++ view/header.php
@@ -17,14 +17,15 @@
 	<!-- <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"> -->
 	<!-- <link rel="stylesheet" type="text/css" href="css/header.css"> -->
 	<!-- <meta name="viewport" content="minimal-ui, width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">  -->
-	<link href='http://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
+	<!-- <link href='http://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'> -->
+	<link type="text/css" rel="stylesheet" href="css/main.css">
 	<title></title>
 </head>
 <body>
 <!-- Header is linked to ligin, register and blog post -->
-<nav class="navbar navbar-default navbar-fixed-top" role="navigation" id= "links">
+<!-- <nav class="navbar navbar-default navbar-fixed-top" role="navigation" id= "links">
   <div class="container">
     <a href="login.php">login</a>
 	<a href="register.php"><br>Register</a>
   </div>
-</nav>
+</nav> -->
